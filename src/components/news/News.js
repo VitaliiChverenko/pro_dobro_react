e12a509068a20ce292c3cdc526160ff778869c2e
Sasha Muliar <alexmason@ukr.net>
2018-05-09 13:56:53 +0300

    develop_PROD-93: added news index page

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -2,30 +2,64 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem'
 import { dbNews } from '../../firebase';
 import CreateNews from './CreateNews';
+import './news-style.css'
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 export default class NewsList extends Component{
   constructor(props){
     super(props)
     this.state = {
-      news: {}
+      news: {},
+      loading: false,
+      loaded: false,
     }
   }
 
   componentDidMount(){
+    this.onUpdateNews();
+  }
+
+  onUpdateNews = () => {
+    this.setState({loading: true})
     dbNews.onceGetNews().then(snapshot => {
-      this.setState(() => ({news: snapshot.val() || {}}));
+      this.setState(() => ({
+        news: snapshot.val() || {},
+        loading: false,
+        loaded: true,
+      }));
     })
+  }
+
+  isEmpty = (obj) => {
+    return Object.getOwnPropertyNames(obj).length === 0 
   }
 
   render(){
     return(
-      <div>
-        {
-          Object.keys(this.state.news).map(key =>
-            <NewsItem event = {this.state.news[key]} key={key}/>
-          )
-        }
-        <CreateNews />
+      <div className="news-wrapper">
+        <Dimmer.Dimmable dimmed={this.state.loading} >
+          <Dimmer active={this.state.loading} inverted>
+            <Loader>Loading</Loader>
+          </Dimmer>
+          { 
+            this.isEmpty(this.state.news) && this.state.loaded ?
+            <div className="ui container">
+              <h2 className="no-news">
+                There are no news yet!
+              </h2>
+              <CreateNews onCreated={this.onUpdateNews} />
+            </div>
+            : 
+            <div className="ui container">
+              {
+                Object.keys(this.state.news).map(key =>
+                  <NewsItem event = {this.state.news[key]} key={key}/>
+                )
+              }
+              <CreateNews onCreated={this.onUpdateNews} />
+            </div>
+          }
+        </Dimmer.Dimmable>
       </div>
     )
   }
--- src/components/news/NewsItem.js
+++ src/components/news/NewsItem.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ShowNews from './ShowNews'
+import placeholder from '../../media/images/pic-placeholder.png';
 
 const NewsItem = (props) => {
   return(
-    <div>
-      <div>{props.event.title}
-        <ShowNews news={props.event}/>
+    <div className="ui grid segment">
+      <div className="five wide column news-pic-wrapper">
+        <div className="news-pic-box" style={{backgroundImage: `url('${placeholder}')`}}></div>
+        <div className="news-pic-box" style={{backgroundImage: `url('${props.event.imageUrl}')`}}></div>
+        <p className="created-at">
+          Created at: <span> {new Date(props.event.createdAt).toLocaleString()} </span>
+        </p>
+      </div>
+      <div className="eleven wide column news-description">
+        <h2>{props.event.title}</h2>
+        <p>
+          {props.event.description}
+        </p>
+        <div className="show-news-btn">
+          <ShowNews news={props.event}/>          
+        </div>
       </div>
     </div>
   )
