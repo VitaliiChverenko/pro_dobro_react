dbc7bee636de66df6a1fddf57bfa0ec2e1b48973
gnaizer <gntnaizer@gmail.com>
2018-05-10 18:32:01 +0300

    change firebase file and record message in REDME.md for everyone

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 pro_dobro_react
+
+Attention!!!
+-we got new file .env
+-for development we using test project on Firebase
+-after that we just have to change credentials for production
+So, we don't need to change nothing in firebase.js from now!
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,19 +9,6 @@ const config = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 
-// const devConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
-//   };
-
-// const config = process.env.NODE_ENV === 'production'
-//   ? prodConfig
-//   : devConfig;
-
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 }
